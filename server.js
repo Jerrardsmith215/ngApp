9e319c39c37dd65368695cfb4b7ed08f4d7bd7b4
jerrardtsmith <jerrard.smith@balconyvisions.com>
2020-04-05 06:29:26 -0400

    correction to static server/json for heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + './dist'));
+app.use(express.static(path.join(__dirname + './dist/ng-app')));
 
 app.get('/*', (req,res) => { 
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/ng-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
