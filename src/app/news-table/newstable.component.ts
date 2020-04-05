d18f50ee283c4072870cb40ef82fff1824c266aa
jerrardtsmith <jerrard.smith@balconyvisions.com>
2020-04-05 03:07:57 -0400

    table clickale

--- src/app/news-table/newstable.component.ts
+++ src/app/news-table/newstable.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PostFetchService } from '../post-fetch.service';
 
-let store = [];
-
 @Component({
     selector: 'news-table',
     templateUrl: 'newstable.component.html',
@@ -12,13 +10,15 @@ let store = [];
 
 export class NewsTableComponent implements OnInit {
     public data = [];
+    public openDialog = (el) => {
+        alert('Link: ' + el.link + '\n\n Content: ' + el.content.rendered);
+    }
     constructor(private api: PostFetchService) { }
 
     ngOnInit() { 
         this.api.fetchPosts()
             .subscribe(posts => {
                 this.data = posts;
-                store = posts;
             });
     }
 
