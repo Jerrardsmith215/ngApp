d6ed914b2f632975185a3e075cbfeb74ec069e01
jerrardtsmith <jerrard.smith@balconyvisions.com>
2020-04-05 00:08:17 -0400

    Added packages and created newTable component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewsTableComponent } from './news-table/newstable.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/news-table/newstable.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'news-table',
+    templateUrl: 'newstable.component.html',
+    styleUrls: ['newstable.component.scss']
+})
+
+export class NewsTableComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
