640b2fde13e4a9b1d12afb59c79255388b0010c3
jerrardtsmith <jerrard.smith@balconyvisions.com>
2020-04-05 06:45:16 -0400

    ...

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname + '/dist/ngapp')));
+app.use(express.static(__dirname + '/dist/ngapp'));
 
 app.get('*', (req,res) => { 
     res.sendFile(path.join(__dirname + '/dist/ngapp/index.html'));
